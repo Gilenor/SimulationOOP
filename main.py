25d803c067948376e1cc852de1b8afaca1a7015c
Sergey <beldur7@gmail.com@>
2026-04-11 15:27:14 +0300

    carried out simple tests to check SpawnEntityAction

--- main.py
+++ main.py
@@ -9,15 +9,45 @@ from entities import Tree, Rock, Grass
 from config import WORLD_WIDTH, WORLD_HEIGHT
 
 world = World2D(WORLD_WIDTH, WORLD_HEIGHT)
-world.add_entity(Grass(), Position(2, 2))
-world.add_entity(Rock(), Position(3, 6))
-world.add_entity(Tree(), Position(5, 8))
-world.add_entity(Rock(), Position(2, 8))
-
+#world.add_entity(Grass(), Position(2, 2))
+#world.add_entity(Rock(), Position(3, 6))
+#world.add_entity(Tree(), Position(5, 8))
+#world.add_entity(Rock(), Position(2, 8))
 
 simulation = Simulation(
     world=world,
     render=ConsoleRender(ConsoleTextSpriteMap()),
 )
+world.add_entity(Grass(), Position(0,0))
+print(world.get_empty_positions())
+print(hash((0,0)) in world._entities)
+print(Position(0,0) in world._entities)
 
-simulation.next_turn()
+
+positions = {}
+pos = Position(0, 0)
+positions[pos] = str(hash(pos))
+positions[(0,0)] = str(hash(pos))
+
+positions = set()
+positions.add(Position(0, 0))
+positions.add((0, 0))
+
+print(positions)
+
+
+print("        (0, 0):", hash((0, 0)))
+print("        (0, 0):", hash((0, 0)))
+print()
+
+print("Position(0, 0):", hash(Position(0, 0)))
+print("Position(0, 0):", hash(Position(0, 0)))
+print()
+
+print("        (0, 0) in positions:", (0, 0) in positions)
+print("Position(0, 0) in positions:", Position(0, 0) in positions)
+print()
+
+
+#simulation.next_turn()
+#simulation.start_simulation()
--- simulation.py
+++ simulation.py
@@ -1,4 +1,8 @@
 import time
+import config
+
+from actions import SpawnEntitiesAction
+from actions.factories import GrassFactory, TreeFactory
 from typing import TYPE_CHECKING, List
 
 if TYPE_CHECKING:
@@ -11,17 +15,30 @@ class Simulation():
     def __init__(self, world: 'World', render: 'Render'):
         self._world = world
         self._render = render
-        self._actions = List['Action']
         self._move_counter = 0
+        self._init_actions: List['Action'] = [
+            SpawnEntitiesAction(TreeFactory, count=10),
+            SpawnEntitiesAction(GrassFactory, count=10),
+        ]
+        self._turn_actions = []
 
     def next_turn(self):
         self._move_counter += 1
         self._render.render(self._world)
 
     def start_simulation(self):
-        while True:
-            self.next_turn()
-            time.sleep(1)
+        self._initialize()
+        self.next_turn()
+
+        #while True:
+        #    self.next_turn()
+        #    time.sleep(1)
 
     def pause_simulation(self):
         pass
+
+    # ---------- private ------------------------------------------------------
+
+    def _initialize(self):
+        for action in self._init_actions:
+            action.execute(self._world)
