12e0221d31177db27ae11e353dce6308667b0f6c
Sergey <beldur7@gmail.com@>
2026-04-11 15:12:10 +0300

    extended the functionality of the class

--- worlds/world_2d.py
+++ worlds/world_2d.py
@@ -1,10 +1,10 @@
 from .world import World
+from position import Position
 
-from typing import TYPE_CHECKING, List, Dict, Tuple
+from typing import TYPE_CHECKING, List, Dict, Tuple, Type
 
 if TYPE_CHECKING:
     from entities import Entity
-    from position import Position
 
 
 class World2D(World):
@@ -14,29 +14,50 @@ class World2D(World):
         self._entities: Dict['Position', 'Entity'] = {}
 
     def add_entity(self, entity: 'Entity', position: 'Position'):
-        if position not in self._entities:
-            self._entities[position] = entity
-        else:
-            raise KeyError(f"This position: {position.get_coords()} was busy")
+        if not self._is_inside(position):
+            raise ValueError("Position out of bounds")
+        if not self._is_free(position):
+            raise ValueError(f"Position: {position.get_coords()} was occupied")
+
+        self._entities[position] = entity
 
     def remove_entity(self, entity: 'Entity'):
-        position = None
-        for p, e in self._entities.items():
+        for pos, e in self._entities.items():
             if entity == e:
-                position = p
+                self._entities.pop(pos)
                 break
 
-        if position:
-            del self._entities[position]
+    def get_entities(self, entity_type: Type | None = None) -> List['Entity']:
+        entities = list(self._entities.values())
 
-    def get_entities(self) -> List['Entity']:
-        return list(self._entities.values())
+        if entity_type is None:
+            return entities
+        return [e for e in entities if isinstance(e, entity_type)]
 
     def get_entity_position(self, entity: 'Entity') -> 'Position':
         for pos, e in self._entities.items():
             if entity == e:
                 return pos
-        raise Exception("Entity was not found in world!")
+        raise ValueError("Entity was not found in world!")
+
+    def get_empty_positions(self) -> List['Position']:
+        positions = [ceil for ceil in self._ceils()
+                     if ceil not in self._entities]
+
+        return positions
 
     def get_dimensions(self) -> Tuple:
         return (self._width, self._height)
+
+    # ---------- private ------------------------------------------------------
+
+    def _is_free(self, position: 'Position') -> bool:
+        return position not in self._entities
+
+    def _is_inside(self, position: 'Position') -> bool:
+        return (0 <= position.x < self._width) and (0 <= position.y < self._height)
+
+    def _ceils(self):
+        for x in range(self._width):
+            for y in range(self._height):
+                yield Position(x, y)
