e333c5b071e1232795d75ce3b1dd2f60ce575c2f
Sergey <beldur7@gmail.com@>
2026-04-21 17:08:11 +0300

    added a method for adding positions

---
+++ src/position.py
@@ -0,0 +1,46 @@
+from typing import Tuple
+
+
+class Position(object):
+    def __init__(self, x: int, y: int):
+        self._x = x
+        self._y = y
+
+    def __str__(self) -> str:
+        return f"(x: {self._x}, y: {self._y})"
+
+    def __repr__(self) -> str:
+        return str(self)
+
+    def __add__(self, value: object) -> 'Position':
+        if isinstance(value, Position):
+            return Position(self.x + value.x, self.y + value.y)
+        if isinstance(value, Tuple):
+            return Position(self.x + value[0], self.y + value[1])
+
+        return NotImplemented
+
+    def __eq__(self, value: object) -> bool:
+        if isinstance(value, Position):
+            return self.get_coords() == value.get_coords()
+        if isinstance(value, Tuple):
+            return self.get_coords() == value
+
+        return NotImplemented
+
+    def __hash__(self) -> int:
+        return hash(self.get_coords())
+
+    def __iter__(self):
+        return iter(self.get_coords())
+
+    def get_coords(self) -> Tuple:
+        return (self._x, self._y)
+
+    @property
+    def x(self) -> int:
+        return self._x
+
+    @property
+    def y(self) -> int:
+        return self._y
