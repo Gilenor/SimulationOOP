bcef4034d277598b5608610f01d9f324c0afee76
Sergey <beldur7@gmail.com@>
2026-04-26 10:18:22 +0300

    optimized the class to speed up operations

--- src/position.py
+++ src/position.py
@@ -1,49 +1,26 @@
-from typing import Tuple
+from typing import Tuple, NamedTuple
 
 
-class Position(object):
-    def __init__(self, x: int, y: int):
-        self._x = x
-        self._y = y
+class Position(NamedTuple):
+    x: int
+    y: int
 
     # def __del__(self):
     #    print(f"{self} id: {id(self)} was deleted")
 
     def __str__(self) -> str:
-        return f"(x: {self._x}, y: {self._y})"
+        return f"(x: {self.x}, y: {self.y})"
 
     def __repr__(self) -> str:
         return str(self)
 
     def __add__(self, value: object) -> "Position":
         if isinstance(value, Position):
-            return Position(self._x + value.x, self._y + value.y)
+            return Position(self.x + value.x, self.y + value.y)
         if isinstance(value, Tuple):
-            return Position(self._x + value[0], self._y + value[1])
+            return Position(self.x + value[0], self.y + value[1])
 
         return NotImplemented
-
-    def __eq__(self, value: object) -> bool:
-        if isinstance(value, Position):
-            return self.get_coords() == value.get_coords()
-        if isinstance(value, Tuple):
-            return self.get_coords() == value
-
-        return NotImplemented
-
-    def __hash__(self) -> int:
-        return hash(self.get_coords())
-
-    def __iter__(self):
-        return iter(self.get_coords())
 
     def get_coords(self) -> Tuple[int, int]:
-        return (self._x, self._y)
-
-    @property
-    def x(self) -> int:
-        return self._x
-
-    @property
-    def y(self) -> int:
-        return self._y
+        return self
